c460e9c6b381959a2c1d3dc8a2acdf4dee90b645
duznevenmadder <keeling.taylor@gmail.com>
2026-06-21 18:43:49 -0700

    Add debug info to check DATABASE_URL

--- files/src/app/page.tsx
+++ files/src/app/page.tsx
@@ -1,5 +1,3 @@
-import { redirect } from "next/navigation";
-
 export default function Home() {
-  redirect("/dashboard");
+  return <div>Database URL is set: {process.env.DATABASE_URL ? 'yes' : 'no'}</div>;
 }
